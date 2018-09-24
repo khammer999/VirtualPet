86127a47c78a1424a26c42354c70e44c6d84c678
khammer999 <khammer999@gmail.com>
2018-09-24 03:50:34 -0400

    20180924 0350hrs

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean appRunState = true; //application run state control variable
+            string checkForExit = "null";
+
+            // create the Virtual pet
+            VirtualPet myPet = new VirtualPet();
+
+            //promt user for name and species for UI
+            Console.WriteLine("Welcome to VirtualPet Please enter a Name for your Pet");
+            myPet.PetName = Console.ReadLine();
+            Console.WriteLine("Please enter a Species for your Pet");
+            myPet.Species = Console.ReadLine();
+
+            int tickCount = 0; //counts until mod 3 == 0 then call Tick()
+
+            do
+            { // run state of program/// option to exit loop.
+
+                
+
+                // display UI
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("{0} the {1} ", myPet.PetName, myPet.Species);
+                Console.WriteLine();
+                Console.WriteLine("Hunger:  " + myPet.Hunger);                                                    // display pet status
+                Console.WriteLine("Thirst:  " + myPet.Thirst);
+                Console.WriteLine("Boredom:  " + myPet.Boredom);
+                Console.WriteLine("Sickness:  " + myPet.Sickness);
+                Console.WriteLine();
+                Console.WriteLine("What would you like to do");
+                Console.WriteLine();
+                Console.WriteLine("< 1 > Feed {0}",myPet.PetName);
+                Console.WriteLine();
+                Console.WriteLine("< 2 > Water {0}",myPet.PetName);
+                Console.WriteLine();
+                Console.WriteLine("< 3 > Play with {0}",myPet.PetName);              
+                Console.WriteLine();
+                Console.WriteLine("< 4 > Do Nothing");
+
+                // prompt menu response
+                int menuSelection = int.Parse(Console.ReadLine());
+
+                //display action and call class methods
+                if (menuSelection == 1)
+                {
+                    Console.WriteLine("You fed {0}", myPet.PetName);
+                    myPet.Food();
+                    tickCount++;
+                }
+                if (menuSelection == 2)
+                {
+                    Console.WriteLine("You watered {0}", myPet.PetName);
+
+                    myPet.Water();
+                    tickCount++;
+                }
+                if (menuSelection == 3)
+                {
+                    Console.WriteLine("You played with {0}", myPet.PetName);
+                    myPet.PlayTime();
+                    tickCount++;
+                }
+               
+                if (menuSelection == 4)
+                {
+                    Console.WriteLine("You did nothing");
+                    tickCount++;
+                }
+
+                if (tickCount % 3 == 0) 
+                {
+                    myPet.Tick();
+                }
+
+
+
+
+
+
+            
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //EXIT PROGRAM OPTION
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to Exit? Enter \"Yes\" or \"No\"");
+                
+                checkForExit = Console.ReadLine();
+                checkForExit = checkForExit.ToLower();
+
+                if (checkForExit.Equals("yes"))
+                {
+                    appRunState = false;
+                    Console.WriteLine();
+                    Console.WriteLine("Goodbye!");
+                    Console.WriteLine();
+                    // good bye message  exit system here !
+                }
+
+                //NO CODE BEYOND HERE DANGER //NO CODE BEYOND HERE DANGER //NO CODE BEYOND HERE DANGER //NO CODE BEYOND HERE DANGER
+
+
+            } while (appRunState);// end exit program loop
+
+        }        
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {   /// fields
+
+        private string petName ="null";
+        private string species = "null";
+        private int hunger = 10;
+        private int thirst = 10;
+        private int waste = 10;
+        private int boredom = 10;
+        private int sickness = 0;
+        private int play = 0;
+
+        // properties
+
+        public string PetName
+        {
+            get { return this.petName; }
+            set { this.petName = value; }
+        }
+
+        public string Species
+        {
+            get { return this.species; }
+            set { this.species = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+
+        public int Boredom
+        {
+            get { return this.boredom; }
+            set { this.boredom = value; }
+        }
+
+        public int Sickness
+        {
+            get { return this.sickness; }
+            set { this.sickness = value; }
+        }
+
+        public int Play
+        {
+            get { return this.play; }
+            set { this.play = value; }
+        }
+
+
+        // Constructors
+
+        public VirtualPet()
+        {
+            //default
+        }
+
+        public VirtualPet(string species)
+        {
+            this.PetName = petName;
+            this.Species = species;
+        }
+
+        public VirtualPet(string petName, string species)
+        {
+            this.PetName = petName;
+            this.Species = species;
+        }
+
+        //  Methods
+
+        public void Food()
+        {
+            this.Hunger--;
+            this.Sickness++;
+
+        }
+        public void Food(int food)
+        {
+            this.Hunger = food;
+            this.Thirst =food;
+        }
+
+        public void Water()
+        {
+            this.Thirst++;
+            this.sickness++;
+
+        }
+        public void Water(int water)
+        {
+            this.Thirst = water;
+
+        }
+
+        public void PlayTime()
+        {
+            this.Boredom--;
+            this.Play++;
+        }
+
+        public void PlayTime(int play)
+        {
+            this.Boredom = play;
+            this.Play = play;
+        }
+
+
+        public void Tick() // inserts the spoiler factor sets internal fields using random
+        {
+           
+            Random spoiler = new Random();
+            int spoilerFactor =  spoiler.Next(1,51);
+            this.Food(spoilerFactor+3);
+            this.Water(spoilerFactor-1);
+            this.PlayTime(spoilerFactor+2);
+
+            Console.Write("You've been Ticked");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
